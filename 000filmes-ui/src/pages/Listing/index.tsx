84994f157d9ab052d0aa03321aa618731dd43931
Michael Trindade <michael.trindade@hotmail.com>
2024-06-05 22:05:15 -0400

    Component MovieStar

--- 000filmes-ui/src/pages/Listing/index.tsx
+++ 000filmes-ui/src/pages/Listing/index.tsx
@@ -1,8 +1,12 @@
+import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination";
 
 function Listing() {
     return(
+        <>
         <Pagination />
+        <MovieStars />
+        </>
     );
 }
 export default Listing;
